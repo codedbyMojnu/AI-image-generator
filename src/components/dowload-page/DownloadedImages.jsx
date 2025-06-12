f917f965eee98fcd160744fcc12cc6d31262786f
codebymojnu <thisismojnu@gmail.com>
2025-06-12 17:59:36 +0600

    updater user experience when no download image found

--- src/components/dowload-page/DownloadedImages.jsx
+++ src/components/dowload-page/DownloadedImages.jsx
@@ -6,9 +6,11 @@ export default function DownloadImages() {
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-2">
-        {downloadImages.map((image, i) => (
-          <Image key={i} src={image?.base64image} seed={image?.seed} />
-        ))}
+        {downloadImages?.length > 0
+          ? downloadImages.map((image, i) => (
+              <Image key={i} src={image?.base64image} seed={image?.seed} />
+            ))
+          : "No image download yet"}
       </div>
     </div>
   );
